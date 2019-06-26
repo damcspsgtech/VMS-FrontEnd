d8c5d22a7500945857317ce78761bd7300faa4b8
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-06-26 18:44:13 +0530

    Stub for Guides.js and Students.js

---
+++ src/views/Guides/Guide.js
@@ -0,0 +1,9 @@
+import React, { Components } from 'react'
+
+export default class Guide extends Components {
+    render() {
+        return (
+            null
+        );
+    }
+}
---
+++ src/views/Students/Student.js
@@ -0,0 +1,9 @@
+import React, { Components } from 'react'
+
+export default class Student extends Components {
+    render() {
+        return (
+            null
+        );
+    }
+}
