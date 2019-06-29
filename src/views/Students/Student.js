c79f5e5cf4f9e838777f1b97579b07ae897f96fe
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-06-29 23:09:36 +0530

    Created Cardwise Student List
    Removed Register
    Moved Avatars

--- src/views/Students/Student.js
+++ src/views/Students/Student.js
@@ -1,9 +1,35 @@
-import { Components } from 'react'
+import React, { Component } from 'react'
+import {
+  Card, CardTitle, CardSubtitle, CardHeader, CardBody,
+  Col, Badge, Media
+} from 'reactstrap'
+import parseColor from '../Colors'
+import logo from '../../assets/img/6.jpg'
 
-export default class Student extends Components {
-    render() {
-        return (
-            null
-        );
-    }
+export default class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: props.key,
+      value: props.value,
+    };
+  }
+
+  render() {
+    return (<React.Fragment>
+      <Col xs="12" sm="6" md="4" lg="3">
+        <Card className={'text-dark bg-' + parseColor(this.state.value[3])}>
+          <CardHeader >
+            <CardTitle>{this.props.value[1]}<Media top right className="float-right" size="10px" src={logo} alt="Hlereo" /></CardTitle>
+            <CardSubtitle>
+              <Badge className="float-left">{this.state.value[0]}</Badge></CardSubtitle>
+          </CardHeader>
+          <CardBody>
+            {this.props.value[2]}
+          </CardBody>
+        </Card>
+      </Col>
+    </React.Fragment >
+    );
+  }
 }
--- src/views/Students/Students.js
+++ src/views/Students/Students.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
-import { Card, CardHeader, CardBody} from 'reactstrap';
+import { Row } from 'reactstrap';
+import Student from "./Student"
+import axios from 'axios';
 
-class Students extends Component {
-    render() {
-        return (
-            <Card>
-                <CardHeader>
-                    Student List
-                </CardHeader>
-                <CardBody>
 
-                </CardBody>
-            </Card>
-        );
+export default class Students extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      student_list: [[]],
     }
-}
+    this.studentCards = this.studentCards.bind(this);
 
-export default Students;
+  }
+
+  componentWillMount() {
+    this.setState({
+      student_list: [
+        ['17PW01', 'Aakash Hemadri', 'Internship Report Filled', 'red'],
+        ['17PW01', 'Aakash Hemadri', 'Internship Report Filled', 'red'],
+        ['17PW01', 'Aakash Hemadri', 'Internship Report Filled', 'red'],
+        ['17PD02', 'Aditiya', 'Internship Report Filled', 'blue'],
+        ['17PD02', 'Aditiya', 'Internship Report Filled', 'blue'],
+        ['17PD02', 'Aditiya', 'Internship Report Filled', 'blue'],
+        ['17PT02', 'Aditiya', 'Internship Report Filled', 'yellow'],
+        ['17PT02', 'Aditiya', 'Internship Report Filled', 'yellow'],
+        ['17PA02', 'Aditiya', 'Internship Report Filled', 'green'],
+        ['17PA02', 'Aditiya', 'Internship Report Filled', 'green'],
+      ]
+    })
+  }
+
+  componenDidMount() {
+    axios.get('/student/list')
+      .then((response) => {
+        this.setState({
+          student_list: response.student_list
+        });
+      })
+      .catch((error) => {
+
+      })
+      .finally()
+  }
+
+  render() {
+    return (
+      <div className="animated fade-in">
+        <Row>
+          {this.studentCards(this.state.student_list)}
+        </Row>
+      </div>
+    );
+  }
+
+  studentCards(items) {
+    return (
+      <React.Fragment >
+        {items.map((item) => <Student key={item[0]} value={item} />)}
+      </React.Fragment >
+    );
+  }
+}
