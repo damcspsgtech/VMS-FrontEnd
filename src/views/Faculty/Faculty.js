cfede4a6bb04a83e3d6b03858a3aecd425283ed5
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-07-02 00:19:42 +0530

    Finalized Student and Faculty List
    Added react-beautiful-dnd

--- src/views/Faculty/Faculty.js
+++ src/views/Faculty/Faculty.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Row } from 'reactstrap';
+import { Row, Col, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import FacultyMember from "./FacultyMember"
 import axios from 'axios';
+import classnames from 'classnames';
 
 
 export default class Students extends Component {
@@ -9,16 +10,18 @@ export default class Students extends Component {
     super(props)
     this.state = {
       faculty_list: [[]],
+      activeTab: 'Faculty'
     }
     this.facultyCards = this.facultyCards.bind(this);
+    this.toggle = this.toggle.bind(this);
 
   }
 
   componentWillMount() {
     this.setState({
       faculty_list: [
-        ['C6027', 'Dr.', 'JEEVADOSS', 'Assistant Professor', 'SJD', 'Mathematics', 'raazdoss@gmail.com', 'Graph Theory', 9600327567, 0, 0],
-        ['C1692', 'Mr.', 'A.MUTHUSAMY', 'Assistant Professor(Selection Grade)', 'AM', 'Mathematics, Computer Science', 'ams.mca@gapps.psgtech.ac.in', "Game theory, Data Mining, Machine learning, Computational Finance, Graph theory, Cryptography", 9442002655, 0, 0],
+        ['C6027', 'Dr.', 'JEEVADOSS', 'Assistant Professor', 'SJD', 'Mathematics', 'raazdoss@gmail.com', 'Graph Theory', 9600327567, 1, 5],
+        ['C1692', 'Mr.', 'A.MUTHUSAMY', 'Assistant Professor(Selection Grade)', 'AM', 'Mathematics, Computer Science', 'ams.mca@gapps.psgtech.ac.in', "Game theory, Data Mining, Machine learning, Computational Finance, Graph theory, Cryptography", 9442002655, 0, 10], ['C6027', 'Dr.', 'JEEVADOSS', 'Assistant Professor', 'SJD', 'Mathematics', 'raazdoss@gmail.com', 'Graph Theory', 9600327567, 1, 7],
       ]
     })
   }
@@ -40,17 +43,51 @@ export default class Students extends Component {
     return (
       <div className="animated fade-in">
         <Row>
-          {this.facultyCards(this.state.faculty_list)}
+          <Col xs="12">
+            <Nav tabs>
+              <NavItem>
+                <NavLink className={classnames({ active: this.state.activeTab === 'Faculty' })} onClick={() => { this.toggle('Faculty') }}>
+                  Faculty
+              </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className={classnames({ active: this.state.activeTab === 'Guides' })} onClick={() => { this.toggle('Guides') }}>
+                  Guides
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <TabContent activeTab={this.state.activeTab}>
+              <TabPane tabId='Faculty' className="animated fade-in">
+                <Row>
+                  {this.facultyCards(this.state.faculty_list)}
+                </Row>
+              </TabPane>
+              <TabPane tabId='Guides'>
+                <Row>
+                  {this.facultyCards(this.state.faculty_list.filter((faculty) => faculty[9] === 1))}
+                </Row>
+              </TabPane>
+            </TabContent>
+          </Col>
         </Row>
-      </div>
+      </div >
     );
   }
 
   facultyCards(items) {
     return (
       <React.Fragment >
-        {items.map((item) => <FacultyMember key={item[0]} value={item} />)}
+        {items.map((item, act) => <FacultyMember key={item[0]} value={item} />)}
       </React.Fragment >
     );
   }
-}
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
+
+} 
--- src/views/Students/Student.js
+++ src/views/Students/Student.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import {
-  Card, CardTitle, CardHeader, CardBody,
-  Col, Badge, Media
+  Card, CardTitle, CardHeader, CardBody, Button,
+  Col, Badge, Media, Modal, ModalHeader, ModalFooter, ModalBody,
+  ListGroup, ListGroupItem, ListGroupItemHeading, InputGroup, InputGroupAddon,
 } from 'reactstrap'
 import parseColor from '../Colors'
 import logo from '../../assets/img/6.jpg'
+import { Link } from 'react-router-dom'
+
 
 export default class Student extends Component {
   constructor(props) {
@@ -12,24 +15,111 @@ export default class Student extends Component {
     this.state = {
       key: props.key,
       value: props.value,
+      info: false,
     };
+    this.toggleInfo = this.toggleInfo.bind(this);
+    this.renderItems = this.renderItems.bind(this);
   }
 
+  toggleInfo() {
+    this.setState({
+      info: !this.state.info
+    });
+  }
   render() {
     return (<React.Fragment>
       <Col xs="12" sm="6" md="4" lg="3">
-        <Card className={'text-dark bg-' + parseColor(this.state.value[3])}>
-          <CardHeader >
-            <CardTitle className="float-right">{this.props.value[1]}<br></br>
-              <Badge className="float-right">{this.state.value[0]}</Badge></CardTitle>
-            <Media top right className="float-left" size="10px" src={logo} alt="Photo" />
-          </CardHeader>
-          <CardBody>
-            {this.props.value[2]}
-          </CardBody>
-        </Card>
+        <Link className="text-decoration-none text-muted:hover" onClick={this.toggleInfo} >
+          <Card className={'text-dark bg-' + parseColor(this.state.value[3])}>
+            <CardHeader >
+              <CardTitle className="float-right">{this.props.value[1]}<br></br>
+                <Badge className="float-right">{this.state.value[0]}</Badge></CardTitle>
+              <Media top right className="float-left" size="10px" src={logo} alt="Photo" />
+            </CardHeader>
+            <CardBody>
+              <InputGroup>
+                <InputGroupAddon>
+                  Report Status
+                </InputGroupAddon>
+                <Button disabled>
+                  Pending
+                </Button>
+              </InputGroup>
+            </CardBody>
+          </Card>
+        </Link>
+        <Modal className={"modal-" + parseColor(this.state.value[3])} isOpen={this.state.info} toggle={this.toggleInfo}>
+          <ModalHeader toggle={this.toggleInfo}>
+            {this.state.value[1]}
+          </ModalHeader>
+          <ModalBody>
+            <ListGroup>
+              {this.renderItems()}
+            </ListGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color={parseColor(this.state.value[3])} onClick={this.toggleInfo}>Okay</Button>
+          </ModalFooter>
+        </Modal>
       </Col>
     </React.Fragment >
+    );
+  }
+
+  renderItems() {
+    return (
+      <React.Fragment>
+        <ListGroupItem>
+          <ListGroupItemHeading>
+            Roll Number
+          </ListGroupItemHeading>
+          <Badge color={parseColor(this.state.value[3])}>
+            <h5>
+              {this.state.value[0]}
+            </h5>
+          </Badge>
+        </ListGroupItem>
+        <ListGroupItem>
+          <ListGroupItemHeading>
+            Project Category
+          </ListGroupItemHeading>
+          <Badge color={parseColor(this.state.value[3])}>
+            <h5>
+
+            </h5>
+          </Badge>
+        </ListGroupItem>
+        <ListGroupItem>
+          <ListGroupItemHeading>
+            Email ID
+          </ListGroupItemHeading>
+          <Badge color={parseColor(this.state.value[3])}>
+            <h5>
+
+            </h5>
+          </Badge>
+        </ListGroupItem>
+        <ListGroupItem>
+          <ListGroupItemHeading>
+            Organization
+          </ListGroupItemHeading>
+          <Badge color={parseColor(this.state.value[3])}>
+            <h5>
+
+            </h5>
+          </Badge>
+        </ListGroupItem>
+        <ListGroupItem>
+          <ListGroupItemHeading>
+            Project Category
+          </ListGroupItemHeading>
+          <Badge color={parseColor(this.state.value[3])}>
+            <h5>
+
+            </h5>
+          </Badge>
+        </ListGroupItem>
+      </React.Fragment>
     );
   }
 }
