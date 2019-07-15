d2ef686c2b8097a66520869d1596faec260df24c
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-07-16 01:39:25 +0530

    Added support for CourseSetting View
    *Added Course using ListGroup
    *Added CRUD for Course views

---
+++ src/views/Settings/Course.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { ListGroupItem, ListGroupItemHeading, Badge, Button } from 'reactstrap';
+
+export default class Course extends Component {
+  render() {
+    return (
+      <div class="animated fadeIn">
+        <ListGroupItem color="info">
+          <ListGroupItemHeading>
+            {this.props.value.name}
+            <Badge pill color="primary">{this.props.value.id}</Badge>
+            <Button className="float-right" color="danger" onClick={this.props.handleDelete.bind(this, this.props.value.id, this.props.value.name)}>
+              Delete
+            </Button>
+          </ListGroupItemHeading>
+        </ListGroupItem>
+      </div>
+    )
+  }
+}
